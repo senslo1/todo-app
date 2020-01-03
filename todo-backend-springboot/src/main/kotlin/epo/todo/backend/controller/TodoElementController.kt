d928e4a3c0ed51245ce09776af7eba0e641b69e9
erik <erikproeis@gmail.com>
2020-01-04 00:30:36 +0100

    Created REST endpoints, service layer and dto

---
+++ todo-backend-springboot/src/main/kotlin/epo/todo/backend/controller/TodoElementController.kt
@@ -0,0 +1,43 @@
+package epo.todo.backend.controller
+
+import epo.todo.backend.model.TodoElementDto
+import epo.todo.backend.service.TodoElementService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TodoElementController(val todoElementService: TodoElementService) {
+
+    @PostMapping(path = ["/todo"])
+    fun create(@RequestBody todoElementDto: TodoElementDto): ResponseEntity<TodoElementDto> {
+        return ResponseEntity.ok(todoElementService.create(todoElementDto))
+    }
+
+    @PutMapping(path = ["/todo/{id}"])
+    fun update(@RequestBody todoElementDto: TodoElementDto,
+               @PathVariable id: Int): ResponseEntity<TodoElementDto> {
+        return ResponseEntity.ok(todoElementService.update(id, todoElementDto))
+    }
+
+    @GetMapping(path = ["/todo"])
+    fun get(@RequestParam category: String?): ResponseEntity<List<TodoElementDto>> {
+        val todos = todoElementService.get(category)
+        return when {
+            todos.isEmpty() -> ResponseEntity.noContent().build()
+            else -> ResponseEntity.ok(todos)
+        }
+    }
+
+    @DeleteMapping(path = ["/todo/{id}"])
+    fun delete(@PathVariable id: Int): ResponseEntity<Unit> {
+        todoElementService.delete(id)
+        return ResponseEntity.ok().build()
+    }
+}
---
+++ todo-backend-springboot/src/main/kotlin/epo/todo/backend/service/TodoElementService.kt
@@ -0,0 +1,46 @@
+package epo.todo.backend.service
+
+import epo.todo.backend.domain.TodoElementEntity
+import epo.todo.backend.exception.BadRequestException
+import epo.todo.backend.model.TodoElementDto
+import epo.todo.backend.repository.TodoElementRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class TodoElementService(val todoElementRepository: TodoElementRepository) {
+
+    fun create(todoElementDto: TodoElementDto): TodoElementDto {
+        return upsert(todoElementDto)
+    }
+
+    fun update(id: Int, todoElementDto: TodoElementDto): TodoElementDto {
+        if (todoElementDto.id == null) {
+            throw BadRequestException("Id of todo element must be specified in the body.")
+        }
+        if (todoElementDto.id != id) {
+            throw BadRequestException("Id of todo element must match the id of the path.")
+        }
+
+        return upsert(todoElementDto)
+    }
+
+    fun get(category: String?): List<TodoElementDto> {
+        val todoElements: List<TodoElementEntity> =
+                when {
+                    category == null -> todoElementRepository.findAll()
+                    else -> todoElementRepository.findAllByCategory(category)
+                }
+        return todoElements.map { TodoElementDto(it) }
+    }
+
+    private fun upsert(todoElementDto: TodoElementDto): TodoElementDto {
+        val todoElementEntity = TodoElementEntity(todoElementDto)
+        return TodoElementDto(todoElementRepository.save(todoElementEntity))
+    }
+
+    fun delete(id: Int) {
+        todoElementRepository.deleteById(id)
+    }
+}
