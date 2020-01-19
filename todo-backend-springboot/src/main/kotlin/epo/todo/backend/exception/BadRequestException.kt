e5e4ff253a0879a823c4f0ed26a4c2795f6018fd
erik <erikproeis@gmail.com>
2020-01-19 23:03:05 +0100

    No need for @ResponseStatus, will be setting status in @ControllerAdvice

--- todo-backend-springboot/src/main/kotlin/epo/todo/backend/exception/BadRequestException.kt
+++ todo-backend-springboot/src/main/kotlin/epo/todo/backend/exception/BadRequestException.kt
@@ -1,10 +1,6 @@
 package epo.todo.backend.exception
 
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
 /**
  * Exception thrown when returning a 400 BAD REQUEST HTTP response.
  */
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 class BadRequestException(message: String) : RuntimeException(message)
