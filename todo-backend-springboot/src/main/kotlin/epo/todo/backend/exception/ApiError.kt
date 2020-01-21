4c72083ce37206b685873f65021acbf8838be3dd
erik <erikproeis@gmail.com>
2020-01-21 22:41:08 +0100

    * Added correlationId to ApiError and removed unused debugMessage * Some refactoring in the Exception handler, plus setting correlation id to error responses

--- todo-backend-springboot/src/main/kotlin/epo/todo/backend/exception/ApiError.kt
+++ todo-backend-springboot/src/main/kotlin/epo/todo/backend/exception/ApiError.kt
@@ -3,13 +3,14 @@ package epo.todo.backend.exception
 import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.http.HttpStatus
 import java.time.Instant
+import java.util.UUID
 
 /**
  * Data class used for custom error response bodies.
  */
 data class ApiError(val status: HttpStatus,
-                    val message: String = "Unexpected error",
-                    var debugMessage: String? = null) {
+                    val message: String,
+                    val correlationId: UUID) {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private val timestamp: Instant = Instant.now()
