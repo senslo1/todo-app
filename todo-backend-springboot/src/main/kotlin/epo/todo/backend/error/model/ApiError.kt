d89dbd6836d1567e07656b8bfa34018a5d836bfb
erik <erikproeis@gmail.com>
2020-01-26 22:43:51 +0100

    Added JsonInclude annotation to ignore null fields.

--- todo-backend-springboot/src/main/kotlin/epo/todo/backend/error/model/ApiError.kt
+++ todo-backend-springboot/src/main/kotlin/epo/todo/backend/error/model/ApiError.kt
@@ -1,12 +1,14 @@
 package epo.todo.backend.error.model
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonInclude
 import java.time.Instant
 import java.util.UUID
 
 /**
  * Data class used for custom error response bodies.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ApiError(val status: String,
                     val message: String,
                     val debugMessage: SwaggerValidationMessageList?,
