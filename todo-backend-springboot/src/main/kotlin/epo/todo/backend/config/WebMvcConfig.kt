f9a72f9ea27ea30915580a6f16ca5495984f17b6
erik <erikproeis@gmail.com>
2020-01-26 22:12:35 +0100

    Added OpenApiValidationInterceptor to web mvc config

--- todo-backend-springboot/src/main/kotlin/epo/todo/backend/config/WebMvcConfig.kt
+++ todo-backend-springboot/src/main/kotlin/epo/todo/backend/config/WebMvcConfig.kt
@@ -1,5 +1,6 @@
 package epo.todo.backend.config
 
+import com.atlassian.oai.validator.springmvc.OpenApiValidationInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -8,8 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
  * A [WebMvcConfigurer] used for adding custom handler interceptors.
  */
 @Configuration
-class WebMvcConfig(val correlationInterceptor: CorrelationInterceptor) : WebMvcConfigurer {
+class WebMvcConfig(val correlationInterceptor: CorrelationInterceptor,
+                   val openApiValidationInterceptor: OpenApiValidationInterceptor) : WebMvcConfigurer {
+
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(correlationInterceptor)
+        registry.addInterceptor(openApiValidationInterceptor)
     }
 }
