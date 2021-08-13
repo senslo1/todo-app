6facf1288548bf5664dfd58231f23f4cd3d10326
erik <erikproeis@gmail.com>
2021-08-13 11:37:48 +0200

    Added interfaces with model definitions

---
+++ todo-frontend-react/src/interfaces/index.ts
@@ -0,0 +1,13 @@
+export interface ITodo {
+    id: number;
+    text: string;
+    category: string;
+}
+
+export interface IApiError {
+    status: string;
+    correlationId: string;
+    message: string;
+    debugMessage?: any;
+    timestamp: string;
+}
