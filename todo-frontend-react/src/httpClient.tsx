5e53e3c2e4043fb0f28e57ada966f971db24004a
erik <erikproeis@gmail.com>
2021-08-13 13:27:08 +0200

    Added function for requesting todos from backend

---
+++ todo-frontend-react/src/httpClient.tsx
@@ -0,0 +1,21 @@
+import { ITodo } from "./interfaces";
+
+const getTodos = async (url: string): Promise<ITodo[]> => {
+    let response = await fetch(url, {
+        method: 'GET'
+    });
+    if (response.status === 204) {
+        return [];
+    } else if (response.ok) {
+        return response.json();
+    } else {
+        throw response.status;
+    }
+};
+
+const getAllTodos = (): Promise<ITodo[]> => {
+    const url = `http://localhost:5000/api/todo`;
+    return getTodos(url);
+};
+
+export default getAllTodos;
